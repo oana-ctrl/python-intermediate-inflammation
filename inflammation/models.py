fb54898edd6ec9dd68053f58f2ed18754f882ddd
jakobVer <jakob.versnjak@gmail.com>
2024-06-24 12:27:00 +0200

    install linter

--- inflammation/models.py
+++ inflammation/models.py
@@ -20,15 +20,27 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    param data: a 2D data array
+    returns: row mean values
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    param data: a 2D data array
+    returns: row max values
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    param data: a 2D data array
+    returns: row min values
+    """
     return np.min(data, axis=0)
