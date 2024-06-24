d8442ab4795f623c3fad7ca722ffd4e8a9969979
oana <wilkending.s@googlemail.com>
2024-06-24 12:39:07 +0200

    style: make pylinter happy

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -6,21 +6,21 @@ import argparse
 from inflammation import models, views
 
 
-def main(args):
+def main(arguments):
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
     - selecting the necessary models and views for the current task
     - passing data between models and views
     """
-    in_files = args.infiles
+    in_files = arguments.infiles
     if not isinstance(in_files, list):
-        in_files = [args.infiles]
+        in_files = [arguments.infiles]
 
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 
+        view_data = {'average': models.daily_mean(inflammation_data),
                      'max': models.daily_max(inflammation_data), 
                      'min': models.daily_min(inflammation_data)}
 
--- inflammation/models.py
+++ inflammation/models.py
@@ -10,7 +10,7 @@ and each column represents a single day across all patients.
 import numpy as np
 
 
-def load_csv(filename):  
+def load_csv(filename):
     """Load a Numpy array from a CSV
 
     :param filename: Filename of CSV to load
@@ -43,4 +43,3 @@ def daily_min(data):
     :returns: minimum along axis 0
     """
     return np.min(data, axis=0)
-
