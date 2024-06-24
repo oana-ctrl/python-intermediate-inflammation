d6455be1684ea203736ea638caaa6b61ffdda11f
oana <wilkending.s@googlemail.com>
2024-06-24 12:21:50 +0200

    doc: add docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,28 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: 2D data array
+    :returns: mean along axis 0
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    
+    :param data: 2D data array
+    :returns: maximum along axis 0
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    
+    :param data: 2D data array
+    :returns: minimum along axis 0
+    """
     return np.min(data, axis=0)
 
